58566296fdd85fa5632d847188fa1261d353a31e
Michael Schoderer <schoderer@proton.me>
2023-12-16 11:45:30 +0100

    Initial Home-Manager

---
+++ home-manager/home.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "michael";
+  home.homeDirectory = "/home/michael";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  home.packages = [
+
+  ];
+  programs = {
+    git = {
+      enable = true;
+      userName = "Michael Schoderer";
+      userEmail = "schoderer@proton.me";
+    };
+  };
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+    ".config/helix/config.toml".text = ''
+      theme = "onedark"
+      [editor]
+      line-number = "relative"
+      color-modes = true
+    '';
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/michael/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "hx";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
