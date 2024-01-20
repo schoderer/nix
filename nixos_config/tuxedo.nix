26c750f95dd22631d59380dbde974ee9f6301f0c
Michael Schoderer <schoderer@proton.me>
2024-01-20 21:39:25 +0100

    Added tuxedo configuration

--- nixos_config/tuxedo.nix
+++ nixos_config/tuxedo.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 {
   imports = [
-    ./hardware/home_pc_configuration.nix
+    ./hardware/tuxedo_configuration.nix
     ./base_configuration.nix
     ./desktop/gnome.nix
     ./programs/common.nix
     ./programs/virtualization.nix
     ./programs/gaming.nix
   ];
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "tuxedo"; # Define your hostname.
   networking.wireless.enable = false; # Enables wireless support via wpa_supplicant.
   services.printing.enable = false; # Printing suppourt
   services.flatpak.enable = true; # Enable Flatpaks
