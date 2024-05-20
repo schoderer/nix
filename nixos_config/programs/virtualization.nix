3beff32c4198bc83ab51fb0f58c18c62b2352f72
Michael Schoderer <github.anemic814@passmail.net>
2024-05-20 22:21:33 +0200

    Update

--- nixos_config/programs/virtualization.nix
+++ nixos_config/programs/virtualization.nix
@@ -3,10 +3,18 @@
   programs.virt-manager.enable = true;
   virtualisation = {
     libvirtd.enable = true;
+
     docker.enable = true;
+    #podman = {
+    #  enable = true;
+    #  dockerCompat = true;
+    #  defaultNetwork.settings.dns_enabled = true;
+    #};
   };
   environment.systemPackages = with pkgs; [
     podman-compose
   ];
   users.groups.libvirtd.members = [ "michael" ];
+  users.extraGroups.vboxusers.members = [ "michael" ];
+  users.extraGroups.docker.members = [ "michael" ];
 }
