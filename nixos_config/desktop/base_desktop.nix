6194405578a38ffdd1a106516d0ca97f4dc2477f
Michael Schoderer <github.anemic814@passmail.net>
2024-05-29 00:39:43 +0200

    Deactivated bluetooth

--- nixos_config/desktop/base_desktop.nix
+++ nixos_config/desktop/base_desktop.nix
@@ -3,20 +3,8 @@
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  hardware.bluetooth = {
-    enable = true;
-    settings = {
-      General = {
-        Experimental = true;
-      };
-    };
-  };
-  services.blueman.enable = true;
-  boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
   boot.plymouth.enable = true; # Enable Plymouth, for encrypted boot
   
-   hardware.opentabletdriver.enable = true;
-
   services.flatpak.enable = true; # Enable Flatpaks
   services.fwupd.enable = true; # Firmware verwaltung und upgrades
   services.pipewire = {
