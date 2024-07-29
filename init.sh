9cffe257ed2738d980c9948aab1563ab5b438d46
Michael Schoderer <github.anemic814@passmail.net>
2024-07-30 00:50:55 +0200

    Update init

--- init.sh
+++ init.sh
@@ -4,9 +4,11 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo --user
 flatpak install -y flathub com.discordapp.Discord
-flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub com.github.johnfactotum.Foliate
 
 flatpak install -y flathub org.kicad.KiCad
 flatpak install -y flathub org.freecadweb.FreeCAD
 flatpak install -y flathub com.prusa3d.PrusaSlicer
+
+flatpak install -y flathub org.inkscape.Inkscape
+flatpak install -y flathub org.kde.krita
