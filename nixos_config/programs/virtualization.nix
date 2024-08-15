5a3288947800d947156a79d1923bc398ef0bbbf7
Michael Schoderer <github.anemic814@passmail.net>
2024-08-15 22:27:49 +0200

    Update switch to docker

--- nixos_config/programs/virtualization.nix
+++ nixos_config/programs/virtualization.nix
@@ -4,16 +4,16 @@
   virtualisation = {
     libvirtd.enable = true;
 
-    #docker.enable = true;
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
+    docker.enable = true;
+    #podman = {
+    #  enable = true;
+    #  dockerCompat = true;
+    #  defaultNetwork.settings.dns_enabled = true;
+    # };
   };
   environment.systemPackages = with pkgs; [
     podman-compose
   ];
   users.groups.libvirtd.members = [ "michael" ];
-  #users.extraGroups.docker.members = [ "michael" ];
+  users.extraGroups.docker.members = [ "michael" ];
 }
