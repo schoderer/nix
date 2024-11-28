42cc5a998ed66c519f49555bf5ba27ce77c07df9
Michael Schoderer <github.anemic814@passmail.net>
2024-11-28 18:26:44 +0100

    Configured zed

--- home-manager/modules/dev-gui.nix
+++ home-manager/modules/dev-gui.nix
@@ -5,13 +5,34 @@
       extensions = [
         "nix"
         "toml"
+        "html"
       ];
       userSettings = {
+        theme = "Andromeda";
         hour_format = "hour24";
         auto_update = false;
+        autosave= "on_focus_change";
+        features = {
+          copilot = true;
+        };
+        load_direnv = "shell_hook";
+        ensure_final_newline_on_save = false;
+        inlay_hints = {
+          enable = true;
+        };
+        assistant = {
+          enabled = true;
+          version = "2";
+          telemetry = false;
+          default_model = {
+            provider = "copilot_chat";
+            model = "gpt-4o";
+          };
+          
+        };
         telemetry ={
-          enable_crash_reporter = false;
-          enable_telemetry = false;
+          metrics = false;
+          diagnostics = false;
         };
       };
    };
