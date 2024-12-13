f180fba52b21cd210a0ab6e22229cc689ac0545f
Michael Schoderer <github.anemic814@passmail.net>
2024-12-13 15:55:58 +0100

    Removed authorized keys

--- home-manager/modules/server.nix
+++ home-manager/modules/server.nix
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 { ... }: {
   home.file = {
     ".ssh/authorized_keys".source = ../dotfiles/authorized_keys;
   };
+=======
+{ config, pkgs, ...}: {
+>>>>>>> Stashed changes
 }
