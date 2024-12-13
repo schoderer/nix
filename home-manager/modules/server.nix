0530f7bfc9080d40bf4de27ea3b9ec0ac5345c7a
Michael Schoderer <github.anemic814@passmail.net>
2024-12-13 16:00:04 +0100

    Update

--- home-manager/modules/server.nix
+++ home-manager/modules/server.nix
@@ -1,9 +1,2 @@
-<<<<<<< Updated upstream
-{ ... }: {
-  home.file = {
-    ".ssh/authorized_keys".source = ../dotfiles/authorized_keys;
-  };
-=======
 { config, pkgs, ...}: {
->>>>>>> Stashed changes
 }
