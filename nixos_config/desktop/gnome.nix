4dca7fc97211c7c0e86bed05e4101f70134e57b7
Michael Schoderer <github.anemic814@passmail.net>
2025-04-02 19:44:51 +0200

    Update

--- nixos_config/desktop/gnome.nix
+++ nixos_config/desktop/gnome.nix
@@ -16,5 +16,6 @@
   ]);
   environment.systemPackages = with pkgs; [
     gnome-software
+    gnome-tweaks
   ];
 }
--- nixos_config/develop.nix
+++ nixos_config/develop.nix
@@ -3,7 +3,7 @@
   imports = [
     ./hardware/develop_configuration.nix
     ./base_configuration.nix
-    ./desktop/budgie.nix
+    ./desktop/gnome.nix
     ./programs/common.nix
     ./programs/virtualization.nix
     ./programs/gaming.nix
