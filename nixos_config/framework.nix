ab1d9bab1d4c076f41102752b2a3bf6e4e0289ad
Michael Schoderer <github.anemic814@passmail.net>
2025-04-09 23:03:20 +0200

    Switch back to gnome on framework

--- nixos_config/framework.nix
+++ nixos_config/framework.nix
@@ -3,7 +3,7 @@
   imports = [
     ./hardware/framework_configuration.nix
     ./base_configuration.nix
-    ./desktop/kde.nix
+    ./desktop/gnome.nix
     ./programs/common.nix
     ./programs/virtualization.nix
     ./programs/gaming.nix
