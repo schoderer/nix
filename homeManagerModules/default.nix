124813be0c80170fe077e8f1a06fe79a4a6a8d62
Michael Schoderer <github.anemic814@passmail.net>
2025-09-09 21:24:20 +0200

    Simplified

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -1,8 +1,13 @@
 {...}: {
+  nixpkgs.config.allowUnfree = true;
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
   imports = [
-    ./user.nix
-    ./modules/base
-    ./modules/dev
+    ./modules/base/user.nix
+    ./modules/base/base.nix
+    ./modules/base/fish-shell.nix
+    ./modules/dev/dev-basic.nix
+    ./modules/dev/dev-kubernetes.nix
     ./modules/gui
   ];
 }
--- homeManagerModules/modules/base/default.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, lib, config, ...}: {
-  nixpkgs.config.allowUnfree = true;
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-  imports = [
-    ./base.nix
-    ./fish-shell.nix
-  ];
-}
