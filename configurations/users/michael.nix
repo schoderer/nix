2e02ff027379db3bb27d03e397ba90ba4d24120a
Michael Schoderer <github.anemic814@passmail.net>
2025-09-11 00:04:30 +0200

    simpilyfied user

--- configurations/users/michael.nix
+++ configurations/users/michael.nix
@@ -1,13 +1,8 @@
 {pkgs, ...}: {
-  programs.fish.enable = true;
   users.users.michael = {
     isNormalUser = true;
     description = "Michael";
     extraGroups = [ "networkmanager" "wheel" "dialout" "libvirtd" ];
-    shell = pkgs.fish;
-    packages = with pkgs; [
-      appimage-run
-    ];
   };
 
 }
