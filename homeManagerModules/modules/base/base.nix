4c98c7c41dcaa79d951a944937f8f4ded184f57d
Michael Schoderer <github.anemic814@passmail.net>
2025-09-21 15:50:58 +0200

    Added global justfile

--- homeManagerModules/modules/base/base.nix
+++ homeManagerModules/modules/base/base.nix
@@ -27,12 +27,12 @@ in {
       ls = "eza --color=always --group-directories-first --icons";
       ll = "eza -la --icons --octal-permissions --group-directories-first";
       l  = "eza -bGF --header --git --color=always --group-directories-first --icons";
-      cat = "bat";
     };
 
     home.file = {
       ".config/helix/config.toml".source = ./dotfiles/helix.toml;
       ".config/helix/languages.toml".source = ./dotfiles/helix_languages.toml;
+      ".config/just/justfile".source = ./dotfiles/global_justfile;
     };
     # Home Manager can also manage your environment variables through
     # 'home.sessionVariables'. If you don't want to manage your shell through Home
