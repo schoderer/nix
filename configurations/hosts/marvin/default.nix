49c298d050dbaf64585b47813823778d6448e7fa
Michael Schoderer <github.anemic814@passmail.net>
2025-10-07 18:43:16 +0200

    New devkey

--- configurations/hosts/marvin/default.nix
+++ configurations/hosts/marvin/default.nix
@@ -11,4 +11,7 @@
     };
     services.openssh.enable = true;
   };
+  users.users.michael.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEPk+r19G1jovsUfe53HBva1iIemANwqZ9h/22cA9p6S devkey"
+  ];
 }
