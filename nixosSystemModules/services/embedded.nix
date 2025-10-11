aacb71bc560faae6e819446becdda11dc54ee8ea
Michael Schoderer <github.anemic814@passmail.net>
2025-10-11 18:29:54 +0200

    Update

--- nixosSystemModules/services/embedded.nix
+++ nixosSystemModules/services/embedded.nix
@@ -11,13 +11,8 @@ in {
       picotool
     ];
     services.udev.extraRules = ''
-
-
-      # https://github.com/raspberrypi/picotool/blob/25aa087b2c517b4901874a99536e869d4d27b678/udev/60-picotool.rules#L4
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="0003", TAG+="uaccess", MODE="660", GROUP="plugdev"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="0009", TAG+="uaccess", MODE="660", GROUP="plugdev"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000a", TAG+="uaccess", MODE="660", GROUP="plugdev"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000f", TAG+="uaccess", MODE="660", GROUP="plugdev"
+      # Allow Access to all RPi Devices
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", TAG+="uaccess", MODE="666", GROUP="plugdev"
     '';
   };
 }
