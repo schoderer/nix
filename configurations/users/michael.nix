6deb4ecd52bfdf9af61b43c0a3d587a00afa9f75
Michael Schoderer <github.anemic814@passmail.net>
2025-12-20 18:48:34 +0100

    Update

--- configurations/users/michael.nix
+++ configurations/users/michael.nix
@@ -4,11 +4,12 @@
     isNormalUser = true;
     description = "Michael";
     shell = pkgs.fish;
-    extraGroups = [ "networkmanager" "wheel" "dialout" "libvirtd" "plugdev" "k8s" ];
+    extraGroups = [ "networkmanager" "wheel" "dialout" "libvirtd" "plugdev" "k8s" "podman" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO10xSRLGOMQgKTMlbIy0GHEBQ1u7Lio/FR5iLaGSxmC michael"
     ];
-
+    subUidRanges = [ { startUid = 100000; count = 65536; } ];
+    subGidRanges = [ { startGid = 100000; count = 65536; } ];
   };
 
 }
--- nixosSystemModules/programs/virtualization.nix
+++ nixosSystemModules/programs/virtualization.nix
@@ -12,12 +12,9 @@ in {
 
       podman = {
         enable = true;
-        dockerCompat = true;
-        defaultNetwork.settings.dns_enabled = true;
       };
     };
     environment.systemPackages = with pkgs; [
-      podman-compose
     ];
   };
 }
